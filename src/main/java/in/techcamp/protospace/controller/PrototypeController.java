f2af418bfbd2b80d39ca69077c2dc4dfd83bcd4c
ksekita <k.sekita@infomart.co.jp>
2026-07-24 16:39:11 +0900

    コード整形
    コード整形しました

--- src/main/java/in/techcamp/protospace/controller/PrototypeController.java
+++ src/main/java/in/techcamp/protospace/controller/PrototypeController.java
@@ -1,7 +1,10 @@
 package in.techcamp.protospace.controller;
 
+import in.techcamp.protospace.dto.PrototypeDetailResponseDto;
+import in.techcamp.protospace.entity.PrototypeEntity;
+import in.techcamp.protospace.form.PrototypeForm;
+import in.techcamp.protospace.service.PrototypeService;
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,57 +14,46 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import in.techcamp.protospace.dto.PrototypeDetailResponseDto;
-import in.techcamp.protospace.entity.PrototypeEntity;
-import in.techcamp.protospace.form.PrototypeForm;
-import in.techcamp.protospace.service.PrototypeService;
-
 @RestController
 @RequestMapping("/api/prototypes")
 public class PrototypeController {
 
-    private final PrototypeService prototypeService;
+  private final PrototypeService prototypeService;
 
-    public PrototypeController(PrototypeService prototypeService) {
-        this.prototypeService = prototypeService;
+  public PrototypeController(PrototypeService prototypeService) {
+    this.prototypeService = prototypeService;
+  }
+
+  // プロトタイプ一覧取得機能
+  @GetMapping("/")
+  public ResponseEntity<List<PrototypeEntity>> getAllPrototypes() {
+    List<PrototypeEntity> prototypes = prototypeService.getAllPrototypes();
+    return ResponseEntity.ok(prototypes);
+  }
+
+  // プロトタイプ詳細データの取得機能
+  @GetMapping("/{id}")
+  public ResponseEntity<PrototypeDetailResponseDto> getPrototypeDetail(
+      @PathVariable("id") Long id) {
+    PrototypeDetailResponseDto response = prototypeService.getPrototypeDetail(id);
+    return ResponseEntity.ok(response);
+  }
+
+  // プロトタイプ投稿機能
+  @PostMapping("/")
+  public ResponseEntity<String> createPrototype(
+      @ModelAttribute PrototypeForm form, Authentication authentication) {
+    try {
+      Long userId = Long.valueOf(authentication.getName());
+
+      prototypeService.createPrototype(form, userId);
+
+      return ResponseEntity.ok("プロトタイプの投稿に成功しました。");
+
+    } catch (Exception e) {
+      // 処理のどこかで問題が発生した際に表示
+      e.printStackTrace();
+      return ResponseEntity.status(500).body("エラーが発生しました: " + e.getMessage());
     }
-
-    // プロトタイプ一覧取得機能
-    @GetMapping("/")
-    public ResponseEntity<List<PrototypeEntity>> getAllPrototypes() {
-        List<PrototypeEntity> prototypes = prototypeService.getAllPrototypes();
-        return ResponseEntity.ok(prototypes);
-    }
-
-    // プロトタイプ詳細データの取得機能
-    @GetMapping("/{id}")
-    public ResponseEntity<PrototypeDetailResponseDto> getPrototypeDetail(
-            @PathVariable("id") Long id) {
-        PrototypeDetailResponseDto response = prototypeService.getPrototypeDetail(id);
-        return ResponseEntity.ok(response);
-    }
-
-    // プロトタイプ投稿機能
-    @PostMapping("/")
-    public ResponseEntity<String> createPrototype(
-            @ModelAttribute PrototypeForm form,
-            Authentication authentication
-    ) {
-        try {
-            Long userId = Long.valueOf(authentication.getName());
-
-            prototypeService.createPrototype(form, userId);
-
-            return ResponseEntity.ok("プロトタイプの投稿に成功しました。");
-
-        } catch (Exception e) {
-            // 処理のどこかで問題が発生した際に表示
-            e.printStackTrace();
-            return ResponseEntity.status(500).body("エラーが発生しました: " + e.getMessage());
-        }
-    }
+  }
 }
-
-
-
-
--- src/main/java/in/techcamp/protospace/mapper/PrototypeMapper.java
+++ src/main/java/in/techcamp/protospace/mapper/PrototypeMapper.java
@@ -1,24 +1,23 @@
 package in.techcamp.protospace.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Options;
-
 import in.techcamp.protospace.entity.PrototypeEntity;
+import java.util.List;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface PrototypeMapper {
-    //新規投稿によるデータベースの追加
-  @Insert("INSERT INTO prototypes (title, catch_copy, concept, image, user_id) VALUES (#{title}, #{catchCopy}, #{concept}, #{image}, #{userId})")
+  // 新規投稿によるデータベースの追加
+  @Insert(
+      "INSERT INTO prototypes (title, catch_copy, concept, image, user_id) VALUES (#{title}, #{catchCopy}, #{concept}, #{image}, #{userId})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(PrototypeEntity prototype);
+  void insert(PrototypeEntity prototype);
 
   @Select("SELECT * FROM prototypes")
-    List<PrototypeEntity> findAll();
-  
+  List<PrototypeEntity> findAll();
+
   // idを使用して検索
   @Select(
       "SELECT id, title, catch_copy AS catchCopy, concept, image, user_id AS userId FROM"
