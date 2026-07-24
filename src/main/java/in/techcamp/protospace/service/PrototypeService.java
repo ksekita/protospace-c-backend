25f1f326379926c42c458c0420fdd999a54eec21
ksekita <k.sekita@infomart.co.jp>
2026-07-24 15:05:59 +0900

    Revert "Feat list view function"

--- src/main/java/in/techcamp/protospace/service/PrototypeService.java
+++ src/main/java/in/techcamp/protospace/service/PrototypeService.java
@@ -1,63 +1,89 @@
 package in.techcamp.protospace.service;
 
+import in.techcamp.protospace.dto.PrototypeDetailResponseDto;
+import in.techcamp.protospace.entity.PrototypeEntity;
+import in.techcamp.protospace.entity.UserEntity;
+import in.techcamp.protospace.exception.ResourceNotFoundException;
+import in.techcamp.protospace.repository.PrototypeRepository;
+import in.techcamp.protospace.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import in.techcamp.protospace.form.PrototypeForm;
+import in.techcamp.protospace.mapper.PrototypeMapper;
 import org.springframework.web.multipart.MultipartFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
-import in.techcamp.protospace.entity.PrototypeEntity;
-import in.techcamp.protospace.form.PrototypeForm;
-import in.techcamp.protospace.mapper.PrototypeMapper;
-import lombok.RequiredArgsConstructor;
-import java.util.List;
-
 @Service
-@RequiredArgsConstructor
 public class PrototypeService {
-    private final PrototypeMapper prototypeMapper;
 
-     public void createPrototype(PrototypeForm form, Long userId) throws Exception {
+  private final PrototypeRepository prototypeRepository;
+  private final UserRepository userRepository;
+  private final PrototypeMapper prototypeMapper;
+
+  public PrototypeService(
+      PrototypeRepository prototypeRepository, UserRepository userRepository,PrototypeMapper prototypeMapper) {
+    this.prototypeRepository = prototypeRepository;
+    this.userRepository = userRepository;
+    this.prototypeMapper=prototypeMapper;
+  }
+
+  // 記事詳細を取得
+  public PrototypeDetailResponseDto getPrototypeDetail(Long id) {
+    PrototypeEntity prototype = prototypeRepository.findById(id);
+    if (prototype == null) {
+      throw new ResourceNotFoundException("プロトタイプが見つかりません");
+    }
+
+    UserEntity user = userRepository.selectById(prototype.getUserId());
+    String userName = (user != null) ? user.getUsername() : null;
+
+    return new PrototypeDetailResponseDto(
+        prototype.getId(),
+        prototype.getTitle(),
+        prototype.getCatchCopy(),
+        prototype.getConcept(),
+        prototype.getImage(),
+        prototype.getUserId(),
+        userName);
+  }
+
+  public void createPrototype(PrototypeForm form, Long userId) throws Exception {
         
      // 画像の保存処理
     MultipartFile imageFile = form.getImage();
     String savedFileName = null;
 
-    if(imageFile != null && !imageFile.isEmpty()){
-        
-    String originalName = imageFile.getOriginalFilename();
+    if (imageFile != null && !imageFile.isEmpty()) {
 
-    if(originalName != null && originalName.contains(".")){
-    String extension = originalName.substring(originalName.lastIndexOf("."));
-    savedFileName = UUID.randomUUID().toString() + extension;
+      String originalName = imageFile.getOriginalFilename();
 
-    Path uploadPath = Paths.get("uploads/").toAbsolutePath().normalize();
-    if (!Files.exists(uploadPath)) {
-        Files.createDirectories(uploadPath);
+      if (originalName != null && originalName.contains(".")) {
+        String extension = originalName.substring(originalName.lastIndexOf("."));
+        savedFileName = UUID.randomUUID().toString() + extension;
+
+        Path uploadPath = Paths.get("uploads/").toAbsolutePath().normalize();
+        if (!Files.exists(uploadPath)) {
+          Files.createDirectories(uploadPath);
         }
 
-    Path filePath = uploadPath.resolve(savedFileName);
-    imageFile.transferTo(filePath);
-    // ここまで
-        }
-      } else{
-                throw new IllegalArgumentException("画像ファイルが選択されていません");
-            }
-
-        // DB保存
-        PrototypeEntity entity = new PrototypeEntity();
-        entity.setTitle(form.getTitle());
-        entity.setCatchCopy(form.getCatchCopy());
-        entity.setConcept(form.getConcept());
-        entity.setImage(savedFileName);
-        entity.setUserId(userId); 
-
-        prototypeMapper.insert(entity);
+        Path filePath = uploadPath.resolve(savedFileName);
+        imageFile.transferTo(filePath);
+        // ここまで
+      }
+    } else {
+      throw new IllegalArgumentException("画像ファイルが選択されていません");
     }
 
-    public List<PrototypeEntity> getAllPrototypes(){
-        return prototypeMapper.findAll();
-    }
+    // DB保存
+    PrototypeEntity entity = new PrototypeEntity();
+    entity.setTitle(form.getTitle());
+    entity.setCatchCopy(form.getCatchCopy());
+    entity.setConcept(form.getConcept());
+    entity.setImage(savedFileName);
+    entity.setUserId(userId);
+
+    prototypeMapper.insert(entity);
+  }
 }
-
