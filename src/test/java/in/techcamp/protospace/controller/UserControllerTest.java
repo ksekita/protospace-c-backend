13fd3091e3048b62e686fefb7750511c45d7ac34
kaneryo0723 <r.kanezuka@infomart.co.jp>
2026-07-28 13:22:58 +0900

    Modified test code
    ・テストコードにprofileについて検証するコードを追加しました。

--- src/test/java/in/techcamp/protospace/controller/UserControllerTest.java
+++ src/test/java/in/techcamp/protospace/controller/UserControllerTest.java
@@ -166,7 +166,8 @@ class UserControllerTest {
           .andExpect(jsonPath("$.id").value(1L))
           .andExpect(jsonPath("$.name").value("テスト太郎"))
           .andExpect(jsonPath("$.position").value("マネージャー"))
-          .andExpect(jsonPath("$.affiliation").value("エンジニア"));
+          .andExpect(jsonPath("$.affiliation").value("エンジニア"))
+          .andExpect(jsonPath("$.profile").value("プロフィール"));
 
       // ArgumentCaptor で AuthService に渡されたリクエスト DTO を検証
       verify(authService).login(loginRequestDtoCaptor.capture());
