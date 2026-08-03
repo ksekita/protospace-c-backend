66314d5309810a0cc77543c7e5472203103c4f86
ksekita <k.sekita@infomart.co.jp>
2026-08-03 14:44:02 +0900

    fix-add-name

--- src/main/java/in/techcamp/protospace/dto/UserPrototypeListDto.java
+++ src/main/java/in/techcamp/protospace/dto/UserPrototypeListDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class UserPrototypeListDto {
   private Long id;
+  private String name;
   private String title;
   private String catchCopy;
   private String image;
