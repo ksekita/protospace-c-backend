e172194e176c939cbf7bce49eafbbadc881125f3
ksekita <k.sekita@infomart.co.jp>
2026-08-05 13:13:04 +0900

    add-validation

--- src/main/java/in/techcamp/protospace/dto/LoginRequestDto.java
+++ src/main/java/in/techcamp/protospace/dto/LoginRequestDto.java
@@ -1,14 +1,17 @@
 package in.techcamp.protospace.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 // ログインしたいユーザーのメールアドレスとパスワードのJSONデータをJavaオブジェクトとして受け取る
 @Data
 public class LoginRequestDto {
   @NotBlank(message = "メールアドレスは必須です")
+  @Size( max = 255, message = "255文字以内で入力してください" )
   private String email;
 
   @NotBlank(message = "パスワードは必須です")
+  @Size( max = 255, message = "パスワードは255文字以内で入力してください" )
   private String password;
 }
